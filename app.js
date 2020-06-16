518b2573e97651fef6de6ff96648c2c01358d047
hafiz77 <abdullahalhafiz7@gmail.com>
2020-06-16 20:34:59 +0600

    add package json

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-// var express = require('express');
-// app = express();
-// app.get('/', (req, res)=>{
-//     res.send('Hello World!');
-// })
+var express = require('express');
+app = express();
+app.get('/', (req, res)=>{
+    res.send('Hello World!');
+})
 
-// app.get('/ok', (req, res)=>{
-//     res.send('Hello Ok!');
-// })
-
-
-console.log('Hello Ok!');
+app.get('/ok', (req, res)=>{
+    res.send('Hello Ok!');
+})
